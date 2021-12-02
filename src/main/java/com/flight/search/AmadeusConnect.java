f3514cece605d8c14f6ae455769e55bbb5a25bef
Nimishasreejaa <A-10172@ibsplc.com>
2021-12-02 16:59:09 +0530

    Changed amedius

--- src/main/java/com/flight/search/AmadeusConnect.java
+++ src/main/java/com/flight/search/AmadeusConnect.java
@@ -5,7 +5,6 @@ import com.amadeus.Amadeus;
 import com.amadeus.Params;
 import com.amadeus.referenceData.Locations;
 import com.amadeus.resources.FlightOfferSearch;
-import com.amadeus.resources.FlightPrice;
 import com.amadeus.resources.Location;
 import com.amadeus.exceptions.ResponseException;
 
