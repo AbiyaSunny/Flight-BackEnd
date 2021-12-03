6c743ae890dd649057ee42fe25918bac91f4a6fb
AbiyaSunny <95266760+AbiyaSunny@users.noreply.github.com>
2021-12-03 09:44:18 +0530

    Update and rename LocationSearchApplication.java to FlightSearchApplication.java

--- src/main/java/com/flight/search/FlightSearchApplication.java
+++ src/main/java/com/flight/search/FlightSearchApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LocationSearchApplication {
+public class FlightSearchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocationSearchApplication.class, args);
