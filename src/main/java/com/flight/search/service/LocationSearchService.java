a9ea4280005adef00fdcc2dd4070e949c68e5a78
Abiya Sunny <A-10105@ibsplc.com>
2021-12-09 14:15:12 +0530

    location endpoints

--- src/main/java/com/flight/search/service/LocationSearchService.java
+++ src/main/java/com/flight/search/service/LocationSearchService.java
@@ -1,7 +1,6 @@
 package com.flight.search.service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import com.amadeus.exceptions.ResponseException;
@@ -16,31 +15,18 @@ public class LocationSearchService {
 	}
 
 	private static List<LocationSearchResponseModel> locationToLocationSearchResponseConverter(Location[] location) {
-		List<LocationSearchResponseModel> locationResponseModel=new ArrayList<LocationSearchResponseModel>();
-		
-		
-		LocationSearchResponseModel[] locationModel = new LocationSearchResponseModel[location.length];
-		
+
+		List<LocationSearchResponseModel> locationResponseModelList = new ArrayList<LocationSearchResponseModel>();
 		for (Location locationObject : location) {
-			locationResponseModel.add(new LocationSearchResponseModel(locationObject.getName(),
+			locationResponseModelList.add(new LocationSearchResponseModel(locationObject.getName(),
 					locationObject.getDetailedName(), locationObject.getIataCode(),
 					locationObject.getAddress().getCityName(), locationObject.getAddress().getCountryName()));
-		
-		}
-		return locationResponseModel;
 
-		/*
-		 * for (int i = 0; i < location.length; i++) { LocationSearchResponseModel obj =
-		 * new LocationSearchResponseModel(); obj.setName(location[i].getName());
-		 * obj.setDetailedName(location[i].getDetailedName());
-		 * obj.setIataCode(location[i].getIataCode());
-		 * obj.setCityname(location[i].getAddress().getCityName());
-		 * obj.setCountryName(location[i].getAddress().getCountryName());
-		 * locationModel[i] = obj; }
-		 */
+		}
+		return locationResponseModelList;
 	}
 
-	public static List<LocationSearchResponseModel>  getLocations(String keyword) throws ResponseException {
+	public static List<LocationSearchResponseModel> getLocations(String keyword) throws ResponseException {
 		return locationToLocationSearchResponseConverter(getLocationsFromAmadeus(keyword));
 	}
 
