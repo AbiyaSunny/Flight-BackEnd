4fa712acdc302d978d6cf9038457c3bbff8f30fd
Nimishasreejaa <95296490+Nimishasreejaa@users.noreply.github.com>
2021-12-10 18:48:20 +0530

    Update AmadeusConnect.java

--- src/main/java/com/flight/amadeusconnection/AmadeusConnect.java
+++ src/main/java/com/flight/amadeusconnection/AmadeusConnect.java
@@ -19,12 +19,12 @@ public class AmadeusConnect {
 		if (returnDate == null) {
 			return amadeus.shopping.flightOffersSearch.get(Params.with("originLocationCode", origin)
 					.and("destinationLocationCode", destination).and("departureDate", departDate).and("adults", adults)
-					.and("max", 1).and("currencyCode", "INR").and("travelClass", travelClass));
+					.and("max", 3).and("currencyCode", "INR").and("travelClass", travelClass));
 		} else {
 			return amadeus.shopping.flightOffersSearch
 					.get(Params.with("originLocationCode", origin).and("destinationLocationCode", destination)
 							.and("departureDate", departDate).and("returnDate", returnDate).and("adults", adults)
-							.and("max", 1).and("travelClass", travelClass).and("currencyCode", "INR"));
+							.and("max", 3).and("travelClass", travelClass).and("currencyCode", "INR"));
 		}
 	}
 }
