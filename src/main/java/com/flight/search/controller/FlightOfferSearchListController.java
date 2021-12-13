144ba91220af21f5bba2e7f604062eab0cfb6834
navaneethasok <asok.navaneeth@gmail.com>
2021-12-13 21:19:19 +0530

    amadeusconnect modified

--- src/main/java/com/flight/search/controller/FlightOfferSearchListController.java
+++ src/main/java/com/flight/search/controller/FlightOfferSearchListController.java
@@ -1,5 +1,6 @@
 package com.flight.search.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,8 @@ import com.flight.search.amadeusconnection.AmadeusConnect;
 
 @RestController
 public class FlightOfferSearchListController {
+	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/flights")
 	public FlightOfferSearch[] flights(@RequestParam(required = true) String origin,
 			@RequestParam(required = true) String destination, @RequestParam(required = true) String departDate,
